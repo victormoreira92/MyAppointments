bc42ad1c18165dc99633c322fcf7bc8c492e8635
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-08-20 10:35:17 -0300

    feat: Creat model User

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+end
