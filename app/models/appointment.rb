66cd857b14c15cfc161beacea780d74c185eb450
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-08-20 10:47:46 -0300

    feat: Create model Appointment

---
+++ app/models/appointment.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: appointments
+#
+#  id              :bigint           not null, primary key
+#  start_time      :datetime         not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  professional_id :bigint           not null
+#  user_id         :bigint           not null
+#
+# Indexes
+#
+#  index_appointments_on_professional_id  (professional_id)
+#  index_appointments_on_user_id          (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (professional_id => professionals.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class Appointment < ApplicationRecord
+  belongs_to :user
+  belongs_to :professional
+  validates :stat
+end
